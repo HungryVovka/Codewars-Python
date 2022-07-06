03bd7f97fb0fead4b527b9896abd4bdb6e70376a
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-07 02:06:59 +0500

    feat: Will_there_be_enough_space?.py

--- 8ky/Will there be enough space.py
+++ 8ky/Will there be enough space.py
@@ -13,6 +13,7 @@
 # If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
 # 
 # Usage Examples:
+#
 # cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 # cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 # -----------------------------------------------------------
