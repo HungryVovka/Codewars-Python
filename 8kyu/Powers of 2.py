c75068a1fd6fff87913f327aaa2a90e8f8289055
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-08 22:21:07 +0500

    feat: 8kuy solutions

---
+++ 8kyu/Powers of 2.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------
+# Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to 
+# n ( inclusive ).
+# 
+# Examples
+# n = 0  ==> [1]        # [2^0]
+# n = 1  ==> [1, 2]     # [2^0, 2^1]
+# n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+# -----------------------------------------------------------
+
+def powers_of_two(n):
+    arr = [1]
+    while n > 0:
+        arr.insert(1, 2**n)
+        n -= 1
+    return (arr)
