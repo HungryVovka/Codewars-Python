09c949b1c09056d3bc1be87691bd782f053e0437
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-08 22:28:05 +0500

    Delete 8 kyu directory

--- 8 kyu/Convert boolean values to strings 'Yes' or 'No'..py
+++
@@ -1,9 +0,0 @@
-# -----------------------------------------------------------
-# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-# -----------------------------------------------------------
-
-def bool_to_word(boolean):
-    if boolean == True:
-        return('Yes')
-    if boolean == False:
-        return('No')
