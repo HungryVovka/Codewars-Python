20b807c85b9428b02111653fd9a306a2dc58a373
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-08 23:10:11 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Function 3 - multiplying two numbers.py
@@ -0,0 +1,6 @@
+# -----------------------------------------------------------
+# Implement a function which multiplies two numbers.
+# -----------------------------------------------------------
+
+def multiply (a, b):
+    return a * b  
