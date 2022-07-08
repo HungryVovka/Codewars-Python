a854b7e424289cdea28dd01217904404974aea37
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-08 19:15:14 +0500

    feat: 8kuy solutions

---
+++ 8kyu/You Can't Code Under Pressure #1.py
@@ -0,0 +1,6 @@
+# -----------------------------------------------------------
+# Code as fast as you can! You need to double the integer and return it.
+# -----------------------------------------------------------
+
+def double_integer(i):
+    return (i*2)
