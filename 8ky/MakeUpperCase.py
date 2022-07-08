c9443b7f95f5d982b15780462e8b0b533ac979e0
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-08 19:03:50 +0500

    Delete 8ky directory

--- 8ky/MakeUpperCase.py
+++
@@ -1,6 +0,0 @@
-# -----------------------------------------------------------
-# Write a function which converts the input string to uppercase.
-# -----------------------------------------------------------
-
-def make_upper_case(s):
-    return s.upper()
