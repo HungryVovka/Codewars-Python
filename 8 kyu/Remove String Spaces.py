a3ffb2aaf44d807d8258caf487d6b96a4a3d50e0
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-08 22:32:29 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Remove String Spaces.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------
+# Simple, remove the spaces from the string, then return the resultant string.
+# -----------------------------------------------------------
+
+def no_space(x):
+    x = x.replace(' ', '')
+    return x
---
+++ 8 kyu/Reversed Strings.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------
+# Complete the solution so that it reverses the string passed into it.
+# 
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+# -----------------------------------------------------------
+
+def solution(string):
+    return string[::-1]
