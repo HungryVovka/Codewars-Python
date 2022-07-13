901fabb3d7079db708ec35f34aa827479538102f
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-13 20:21:58 +0500

    feat: 8 kyu solutions

--- 8 kyu/L1 Set Alarm.py
+++ 8 kyu/L1 Set Alarm.py
@@ -3,7 +3,7 @@
 # parameter, vacation is true whenever you are on vacation.
 # 
 # The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an 
-# 	alarm). It should return false otherwise. Examples:
+# alarm). It should return false otherwise. Examples:
 # 
 # setAlarm(true, true) -> false
 # setAlarm(false, true) -> false
