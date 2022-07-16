6c4f2512575d8bba74b13b32226e1adaaf0ba8ff
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-16 23:40:09 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Grasshopper - Variable Assignment Debug.py
@@ -0,0 +1,8 @@
+# -----------------------------------------------------------
+# Variable assignment
+# Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+# -----------------------------------------------------------
+
+a = "dev"
+b = "Lab"
+name = a + b
