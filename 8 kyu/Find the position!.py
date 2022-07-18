56b0018b6a541ad6f1a7285f871f0a6d30c11914
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-19 02:53:39 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Find the position!.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------
+# When provided with a letter, return its position in the alphabet.
+# 
+# Input :: "a"
+# 
+# Ouput :: "Position of alphabet: 1"
+# -----------------------------------------------------------
+
+def position(alphabet):
+    return f"Position of alphabet: {ord(alphabet) - 96}"
