ec7c275897bfd29e0cd4b12fd4210777b9753656
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-18 10:31:07 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Beginner Series #3 Sum of Numbers.py
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------
+# Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two 
+# numbers are equal return a or b.
+# 
+# Note: a and b are not ordered!
+# 
+# Examples (a, b) --> output (explanation)
+# (1, 0) --> 1 (1 + 0 = 1)
+# (1, 2) --> 3 (1 + 2 = 3)
+# (0, 1) --> 1 (0 + 1 = 1)
+# (1, 1) --> 1 (1 since both are same)
+# (-1, 0) --> -1 (-1 + 0 = -1)
+# (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+# -----------------------------------------------------------
+
+def get_sum(a,b):
+    maxnum = max(a, b)
+    minnum = min(a, b)
+    return (1 + maxnum - minnum) * (maxnum + minnum) / 2
---
+++ 7 kyu/You're a square!.py
@@ -0,0 +1,39 @@
+# -----------------------------------------------------------
+# A square of squares
+# You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square 
+# building blocks!
+# 
+# However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a 
+# way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+# 
+# Task
+# Given an integral number, determine if it's a square number:
+# 
+# In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer 
+# with itself.
+# 
+# The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+# 
+# Examples
+# -1  =>  false
+#  0  =>  true
+#  3  =>  false
+#  4  =>  true
+# 25  =>  true
+# 26  =>  false
+# -----------------------------------------------------------
+
+def is_square(n):
+    if n < 0:
+        return False
+    if n == 0:
+        return True
+    n1, n2 = 1, n
+    while n1 + 1 < n2:
+        n3 = (n1 + n2) // 2
+        if n3**2 < n:
+            n1 = n3
+        else:
+            n2 = n3
+    answer = (n == n1**2 or n == (n1 + 1)**2)
+    return answer
