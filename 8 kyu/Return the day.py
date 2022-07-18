1903437743b1abfd6d8305a61c8f616a3709fb7d
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-18 06:44:20 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Return the day.py
@@ -0,0 +1,27 @@
+# -----------------------------------------------------------
+# Complete the function which returns the weekday according to the input number:
+# 
+# 1 returns "Sunday"
+# 2 returns "Monday"
+# 3 returns "Tuesday"
+# 4 returns "Wednesday"
+# 5 returns "Thursday"
+# 6 returns "Friday"
+# 7 returns "Saturday"
+# Otherwise returns "Wrong, please enter a number between 1 and 7"
+# -----------------------------------------------------------
+
+def whatday(num):
+    week = {
+        1 : "Sunday",
+        2 : "Monday",
+        3 : "Tuesday",
+        4 : "Wednesday",
+        5 : "Thursday",
+        6 :  "Friday",
+        7 : "Saturday"
+    }
+    try:
+        return week[num]
+    except KeyError:
+        return "Wrong, please enter a number between 1 and 7"
