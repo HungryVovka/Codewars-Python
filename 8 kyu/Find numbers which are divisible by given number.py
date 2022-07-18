f2471f90c43557b49621524d6a8f6c05cafcaec7
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-18 06:52:19 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Find numbers which are divisible by given number.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------
+# Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of 
+# numbers and the second is the divisor.
+# 
+# Example(Input1, Input2 --> Output)
+# [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+# -----------------------------------------------------------
+
+def divisible_by(numbers, divisor):
+    answer = []
+    for i in numbers:
+        if i % divisor == 0:
+            answer.append(i)
+    return answer
