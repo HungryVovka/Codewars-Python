846b6a7b29cad3ee1d3dfb5e95ab22d8c9ffbf30
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-18 09:12:58 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Get decimal part of the given number.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------
+# Write a function that returns only the decimal part of the given number.
+# 
+# You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+# 
+# Examples
+# getDecimal(2.4)  === 0.4
+# getDecimal(-0.2) === 0.2
+# -----------------------------------------------------------
+
+def get_decimal(n): 
+    return abs(n) % 1
