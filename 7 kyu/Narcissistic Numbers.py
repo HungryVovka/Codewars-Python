41d929d76a838d4ec135fc087c5a7188fcdf6905
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-18 09:56:04 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Narcissistic Numbers.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------
+# A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. If this seems 
+# confusing, refer to the example below.
+# 
+# Ex: 153, where l = 3 ( the number of digits in 153 )
+# 13 + 53 + 33 = 153
+# 
+# Write a function that, given n, returns whether or not n is a Narcissistic Number.
+# -----------------------------------------------------------
+
+def is_narcissistic(i):
+    str_i = str(i)
+    i_arrstr = list(str_i)
+    number = 0
+    for n in i_arrstr:
+        n = int(n)
+        number += n**len(i_arrstr)
+    if i == number:
+        return(True)
+    else:
+        return(False)
---
+++ 7 kyu/Sort by Last Char.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------
+# Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+# 
+# If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+# 
+# All inputs will be valid.
+# -----------------------------------------------------------
+
+def last(s):
+    rev_s = s.split(' ')
+    rev_s.sort(key = lambda s:s[-1])
+    return(rev_s)
