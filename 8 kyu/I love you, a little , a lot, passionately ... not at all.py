35b55f8c61ef7c6f2fc14a00978a9efb8ca6c7d0
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-07-26 22:46:48 +0500

    feat: 8 kyu solutions

--- 8 kyu/I love you, a little , a lot, passionately ... not at all.py
+++ 8 kyu/I love you, a little , a lot, passionately ... not at all.py
@@ -8,6 +8,7 @@
 # passionately
 # madly
 # not at all
+# 
 # When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 # 
 # Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
