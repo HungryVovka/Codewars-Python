12060070898cc62ce8b14145a3f3418ec8f238c5
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-08 03:58:15 +0500

    feat: 6 kyu solutions

--- 6 kyu/Unique In Order.py
+++ 6 kyu/Unique In Order.py
@@ -4,9 +4,9 @@
 # 
 # For example:
 # 
-# uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-# uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
-# uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
 # -----------------------------------------------------------
 
 def unique_in_order(iterable):
