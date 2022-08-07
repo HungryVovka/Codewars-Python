c1def55620459a4dea8029ea177e934cfdfe192e
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-07 23:50:31 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Basic Training Add item to an Array.py
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------
+# Add the value "codewars" to the websites array.
+# After your code executes the websites array should == ["codewars"]
+# 
+# The websites array has already been defined for you using the following code:
+# 
+# websites = []
+# -----------------------------------------------------------
+
+websites.append("codewars")
+
+# or
+
+websites.extend(["codewars"])
+
+# or
+
+websites.insert(0, "codewars")
+
+# or
+
+websites += ["codewars"]
