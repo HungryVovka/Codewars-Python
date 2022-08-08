c0749b2d43c01ab89e9858c4167021c11cf0637e
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-09 03:28:57 +0500

    feat: 6 kyu solutions

--- 6 kyu/Array.diff.py
+++ 6 kyu/Array.diff.py
@@ -3,10 +3,10 @@
 # 
 # It should remove all values from list a, which are present in list b keeping their order.
 # 
-# arrayDiff([1,2],[1]) == [2]
+# array_diff([1,2],[1]) == [2]
 # If a value is present in b, all of its occurrences must be removed from the other:
 # 
-# arrayDiff([1,2,2,2,3],[2]) == [1,3]
+# array_diff([1,2,2,2,3],[2]) == [1,3]
 # -----------------------------------------------------------
 
 def array_diff(a, b):
