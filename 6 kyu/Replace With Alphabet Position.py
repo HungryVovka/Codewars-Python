f7b9367882d8acfff230ddbe96e59d1aec6c167c
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-09 03:35:57 +0500

    feat: 6 kyu solutions

--- 6 kyu/Replace With Alphabet Position.py
+++ 6 kyu/Replace With Alphabet Position.py
@@ -8,7 +8,8 @@
 # "a" = 1, "b" = 2, etc.
 # 
 # Example
-# alphabetPosition("The sunset sets at twelve o' clock.")
+# alphabet_position("The sunset sets at twelve o' clock.")
+#
 # Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 # -----------------------------------------------------------
 
