0b1210ac0e4c8cf22a57a5349a7b0861f65800d8
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-10 02:31:54 +0500

    feat: 7 kyu solutions

--- 7 kyu/Get decimal part of the given number.py
+++ 7 kyu/Get decimal part of the given number.py
@@ -4,8 +4,8 @@
 # You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
 # 
 # Examples
-# getDecimal(2.4)  === 0.4
-# getDecimal(-0.2) === 0.2
+# get_decimal(2.4)  # 0.4
+# get_decimal(-0.2) # 0.2
 # -----------------------------------------------------------
 
 def get_decimal(n): 
