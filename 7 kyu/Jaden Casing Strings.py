f94d33bb8eae55772c7974f892748154ac731dd2
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-13 01:13:37 +0500

    feat: 7 kyu solutions

--- 7 kyu/Jaden Casing Strings.py
+++ 7 kyu/Jaden Casing Strings.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------
 # Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his 
 # philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to 
-# capitalize each word, check out how contractions are expected to be in the example below.
+# capitalize each word, check out how contractions are expected to be in the example below.(https://twitter.com/jaden)
 # 
 # Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not 
 # capitalized in the same way he originally typed them.
@@ -11,7 +11,7 @@
 # Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 # Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 # Link to Jaden's former Twitter account @officialjaden via archive.org 
-# (https://web.archive.org/web/20190624190255/https://twitter.com/officialjaden)
+# (https://web.archive.org/web/20190624190255/ https://twitter.com/officialjaden)
 # -----------------------------------------------------------
 
 def to_jaden_case(string):
