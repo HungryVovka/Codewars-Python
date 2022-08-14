929d9b95286e45cfc82c9d697fa98dff97269bad
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-14 16:08:18 +0500

    feat: 8 kyu solutions

--- 8 kyu/Welcome!.py
+++ 8 kyu/Welcome!.py
@@ -8,23 +8,24 @@
 # default to English if the language is not in the database, or in the event of an invalid input.
 # 
 # The Database
-# english: 'Welcome',
-# czech: 'Vitejte',
-# danish: 'Velkomst',
-# dutch: 'Welkom',
-# estonian: 'Tere tulemast',
-# finnish: 'Tervetuloa',
-# flemish: 'Welgekomen',
-# french: 'Bienvenue',
-# german: 'Willkommen',
-# irish: 'Failte',
-# italian: 'Benvenuto',
-# latvian: 'Gaidits',
-# lithuanian: 'Laukiamas',
-# polish: 'Witamy',
-# spanish: 'Bienvenido',
-# swedish: 'Valkommen',
-# welsh: 'Croeso'
+# 'english': 'Welcome',
+# 'czech': 'Vitejte',
+# 'danish': 'Velkomst',
+# 'dutch': 'Welkom',
+# 'estonian': 'Tere tulemast',
+# 'finnish': 'Tervetuloa',
+# 'flemish': 'Welgekomen',
+# 'french': 'Bienvenue',
+# 'german': 'Willkommen',
+# 'irish': 'Failte',
+# 'italian': 'Benvenuto',
+# 'latvian': 'Gaidits',
+# 'lithuanian': 'Laukiamas',
+# 'polish': 'Witamy',
+# 'spanish': 'Bienvenido',
+# 'swedish': 'Valkommen',
+# 'welsh': 'Croeso'
+# 
 # Possible invalid inputs include:
 # 
 # IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
