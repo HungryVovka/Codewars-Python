e3189998f5609e26a2cff73e72b6e6a645730660
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-21 13:50:51 +0500

    feat: 8 kyu solutions

--- 8 kyu/Palindrome Strings.py
+++ 8 kyu/Palindrome Strings.py
@@ -1,6 +1,7 @@
 # -----------------------------------------------------------
 # Palindrome strings
-# A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+# A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, 
+# punctuation, and word dividers.
 # 
 # Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
 # 
