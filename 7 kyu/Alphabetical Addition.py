35ecb30e4f7435ed86d0a81666af89ff4bbfd7eb
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-09-17 23:17:10 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Alphabetical Addition.py
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------
+# Your task is to add up letters to one letter.
+# 
+# The function will be given a variable amount of arguments, each one being a letter to add.
+# 
+# Notes:
+# Letters will always be lowercase.
+# Letters can overflow (see second to last example of the description)
+# If no letters are given, the function should return 'z'
+# 
+# Examples:
+# add_letters('a', 'b', 'c') = 'f'
+# add_letters('a', 'b') = 'c'
+# add_letters('z') = 'z'
+# add_letters('z', 'a') = 'a'
+# add_letters('y', 'c', 'b') = 'd' # notice the letters overflowing
+# add_letters() = 'z'
+# -----------------------------------------------------------
+
+def add_letters(*letters):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    letters_count = 25
+    for letter in letters:
+        letters_count += alphabet.index(letter) + 1
+    return alphabet[letters_count % 26]
