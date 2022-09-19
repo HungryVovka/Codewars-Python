23e9d15aa1126c8c545a1427e8f7336b63a2ad36
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-09-20 03:46:15 +0500

    feat: 5 kyu solutions

---
+++ 5 kyu/Multi Line Task Hello World (Easy one).py
@@ -0,0 +1,49 @@
+# -----------------------------------------------------------
+# You are provided with the following functions:
+# 
+# t = type
+# k = lambda x: lambda _: x
+# 
+# Write a function f that returns the string Hello, world!.
+# 
+# The rules are:
+# 
+# The solution should not exceed 33 lines
+# Every line should have at most 2 characters
+# 
+# Note: There is the same kata for this task on JS, but way harder than Python one, so it is the best option to create a separated kata with fair rank.
+# -----------------------------------------------------------
+
+f\
+=\
+t(
+""
+,(
+),
+{
+'\
+_\
+_\
+n\
+e\
+w\
+_\
+_\
+':
+k(
+"\
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!"
+)}
+)
