3105e77f9a6c843c72145651488d6539f1b8ca0d
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-07 23:40:04 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Multiple of index.py
@@ -0,0 +1,27 @@
+# -----------------------------------------------------------
+# Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+# 
+# Some cases:
+# [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+# 
+# [68, -1, 1, -7, 10, 10] => [-1, 10]
+# 
+# [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+# -----------------------------------------------------------
+
+def multiple_of_index(arr):
+    new_arr = []
+    for i in range(1, len(arr)):
+        if arr[i] % i == 0:
+            new_arr.append(arr[i])
+    return new_arr
+
+# or
+
+def multiple_of_index(arr):
+    return list(arr[i] for i in range(1, len(arr)) if arr[i] % i == 0)
+
+# or
+
+def multiple_of_index(arr):
+    return [j for i, j in enumerate(arr) if i and j % i == 0]
