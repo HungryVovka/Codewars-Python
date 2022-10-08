fc55acff53e23230edef76944c117b64a0b7ffac
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-09 04:04:01 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Is the date today.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------
+# Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+# 
+# Make sure that your function does not return a false positive by only checking the day.
+# -----------------------------------------------------------
+
+from datetime import datetime
+
+def is_today(date): 
+    return date.date() == datetime.today().date()
---
+++ 8 kyu/Merging sorted integer arrays (without duplicates).py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------
+# Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not 
+# have any duplicate.
+# -----------------------------------------------------------
+
+def merge_arrays(first, second):
+    return sorted(set(first + second))
