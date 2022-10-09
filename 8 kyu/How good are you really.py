9ae9835d8cf8c424b3ab39ca4a13f3a619b77365
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-09 22:54:22 +0500

    feat: 8 kyu solutions

--- 8 kyu/How good are you really.py
+++ 8 kyu/How good are you really.py
@@ -12,13 +12,4 @@
 
 def better_than_average(class_points, your_points):
     average = sum(class_points) / len(class_points)
-    if your_points > average:
-        return(True)
-    else:
-        return(False)
-
-# or
-
-def better_than_average(class_points, your_points):
-    average = sum(class_points) / len(class_points)
     return your_points > average
