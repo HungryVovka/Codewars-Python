7c2d81f78ea738ae08752147439756ad2f4d459c
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-10 01:06:51 +0500

    feat: 8 kyu solutions

--- 8 kyu/Reversed sequence.py
+++ 8 kyu/Reversed sequence.py
@@ -11,3 +11,13 @@ def reverse_seq(n):
         n_arr.append(p)
         p -= 1
     return(n_arr)
+
+# or
+
+def reverse_seq(n):
+    return [i for i in range(n, 0, -1)]
+
+# or
+
+def reverse_seq(n):
+    return list(range(n, 0, -1))
