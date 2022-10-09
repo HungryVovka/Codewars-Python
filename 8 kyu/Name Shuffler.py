3f9b6422972287721298d9be0fa3f95f61ccea39
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-10 01:07:45 +0500

    feat: 8 kyu solutions

--- 8 kyu/Name Shuffler.py
+++ 8 kyu/Name Shuffler.py
@@ -9,3 +9,13 @@
 def name_shuffler(str_):
     arr = str_.split()
     return(arr[1] + " " + arr[0])
+
+# or
+
+def name_shuffler(str_):
+    return " ".join(reversed(str_.split()))
+
+# or
+
+def name_shuffler(str_):
+    return " ".join(str_.split()[::-1])
