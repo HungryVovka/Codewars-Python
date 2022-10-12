c2d51717df0c4b5d5a8bfe78faea2bd0f6c3fa12
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-12 23:41:53 +0500

    feat: 7 kyu solutions

--- 7 kyu/All unique.py
+++ 7 kyu/All unique.py
@@ -5,7 +5,4 @@
 # -----------------------------------------------------------
 
 def has_unique_chars(string):
-    if len(string) == len(set(string)):
-        return True
-    else:
-        return False
+    return len(string) == len(set(string))
