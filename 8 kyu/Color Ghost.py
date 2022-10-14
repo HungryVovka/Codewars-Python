03a49ee098fed75dac4c0e0653d6b2d6e11f505b
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-14 12:17:41 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Color Ghost.py
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------
+# Color Ghost
+# Create a class Ghost
+# 
+# Ghost objects are instantiated without any arguments.
+# 
+# Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+# 
+# ghost = Ghost()
+# ghost.color  #=> "white" or "yellow" or "purple" or "red"
+# -----------------------------------------------------------
+
+import random
+
+color_ghost = ["purple", "red", "white", "yellow"]
+
+class Ghost(object):
+    def __init__(self):
+        self.color = random.choice(color_ghost)
---
+++ 8 kyu/Switch Case - Bug Fixing #6.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------
+# Switch/Case - Bug Fixing #6
+# 
+# Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+# -----------------------------------------------------------
+
+def eval_object(v):
+    operation = {
+        "+": v["a"] + v["b"],
+        "-": v["a"] - v["b"],
+        "/": v["a"] / v["b"],
+        "*": v["a"] * v["b"],
+        "%": v["a"] % v["b"],
+        "**": v["a"] ** v["b"],
+    }
+    return operation.get(v["operation"])
