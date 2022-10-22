1d342fbf4bf8553473d9b767e34c01b9e2fcddcb
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-23 01:58:50 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Enumerable Magic #25 - Take the First N Elements.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------
+# Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+# 
+# If you need help, here's a reference:
+# 
+# https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+# -----------------------------------------------------------
+
+def take(arr, n):
+    return arr[:n]
