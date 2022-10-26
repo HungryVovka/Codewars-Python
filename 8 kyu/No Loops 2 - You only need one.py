59f4f422e2a79ed16e640276d235da8300c4de4d
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-27 01:03:42 +0500

    feat: 8 kyu solutions

--- 8 kyu/No Loops 2 - You only need one.py
+++ 8 kyu/No Loops 2 - You only need one.py
@@ -1,16 +1,14 @@
 # -----------------------------------------------------------
 # *** No Loops Allowed ***
 # 
-# You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+# You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
 # 
-# Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account 
-# for case.
+# Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to 
+# account for case.
 # 
 # Looking for more, loop-restrained fun? Check out the other kata in the series:
 # 
-# https://www.codewars.com/kata/no-loops-1-small-enough
-# 
-# https://www.codewars.com/kata/no-loops-3-copy-within
+# No Loops 1 - Small enough?
 # -----------------------------------------------------------
 
 def check(arr, item):
