e603bea4225f0dd9870c299f012ec75ec6f001a3
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-29 23:17:40 +0500

    feat: 8 kyu solutions

--- 8 kyu/Grader.py
+++ 8 kyu/Grader.py
@@ -2,7 +2,7 @@
 # Create a function that takes a number as an argument and returns a grade based on that number.
 # 
 # Score											Grade
-# Anything greater than 1 or less than 0.6		"F"
+# Anything greater than 1 or less than 0.6		                                "F"
 # 0.9 or greater									"A"
 # 0.8 or greater									"B"
 # 0.7 or greater									"C"
