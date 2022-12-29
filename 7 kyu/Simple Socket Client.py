b3694deb604f9641af129ec43b6f4abd760ab22c
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2022-12-29 23:43:04 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Simple Socket Client.py
@@ -0,0 +1,40 @@
+# -----------------------------------------------------------
+# There is a socket listening on port 1111 of local host.
+# 
+# The socket either belongs to a server that sends back anything you send to it, or to a server that reverses anything you send to it.
+# 
+# Create a function that does the following:
+# 
+# Connects to the socket on port 1111.
+# Sends one packet to the server.
+# Receives one packet from the server.
+# Returns True if the server is the regular type (i.e., it sends back the same packet that was sent to it), or False if the server is the reversing type 
+# (i.e., it reverses the packet that was sent to it).
+# 
+# Make sure to close the socket after you are done using it. If you time out while trying to connect, it is likely that you did not connect, send, receive, 
+# and close the socket in the correct order.
+# 
+# Happy coding!
+# -----------------------------------------------------------
+
+import socket
+
+def socket_client():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect(("127.0.0.1", 1111))
+    server.send(b"HungryVovka")
+    response = server.recv(1024)
+    server.close()
+    return response == b"HungryVovka"
+
+# or
+
+import socket
+
+def socket_client():
+    packet = b"HungryVovka"
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.connect(("127.0.0.1", 1111))
+        server.send(packet)
+        response = server.recv(1024)
+        return response == packet
