7ad2da2d8057200c96d60f479471aef07938c8b2
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-01-10 00:29:09 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Perpendicular lines.py
@@ -0,0 +1,11 @@
+# -----------------------------------------------------------
+# You are given an input (n) which represents the amount of lines you are given, your job is to figure out what is the maximum amount of perpendicular 
+# bisectors you can make using these lines.
+# 
+# Note: A perpendicular bisector is one that forms a 90 degree angle
+# 
+# n will always be greater than or equal to 0
+# -----------------------------------------------------------
+
+def perpendicular(n):
+    return n**2 // 4
---
+++ 7 kyu/Summation Triangle #1.py
@@ -0,0 +1,45 @@
+# -----------------------------------------------------------
+# The task
+# You have to make a program capable of returning the sum of all the elements of a triangle with side of size n+1n+1n+1.
+# 
+# The problem
+# Your solution has to support 0 <= n≤ <= 10^6 Brute-forcing will not work!
+# 
+# The definition
+# A triangle element aij where i is the column and j is the row can be defined as aij = 2j + i + 1 where 0 <= j <= i <= n
+# 
+# Examples
+# For n = 2:
+# 
+# 1  2  3      \
+#    4  5       \__ 1+2+3+4+5+7 sums to: 22
+#       7       /
+# sum(2) = 22
+# 
+# For n = 3:
+# 
+# 1  2  3  4   \
+#    4  5  6    \__ 1+2+3+4+4+5+6+7+8+10 sums to: 50
+#       7  8    /
+#         10   /
+# sum(3) = 50
+# 
+# Hints
+# Euler transform (Optional)
+# Sums of powers
+# -----------------------------------------------------------
+
+import numpy as np
+
+def get_sum(n):
+    n += 1
+    x = np.arange(1, n + 1)
+    x = x**2 * 2 - x
+    return x.sum()
+
+# or
+
+def get_sum(n):
+    n += 1
+    r = 6
+    return (n**2 * 3 + n**3 * (r - 2) - n * (r - 5)) // r
