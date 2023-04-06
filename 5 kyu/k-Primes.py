27287a02bccca271293d63f656ddec3ceb81d3b4
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-06 16:24:07 +0500

    feat: 5 kyu solutions

--- 5 kyu/k-Primes.py
+++ 5 kyu/k-Primes.py
@@ -51,3 +51,24 @@ def puzzle(s):
     u = count_Kprimes(3, 0, s)
     z = count_Kprimes(7, 0, s)
     return sum(1 for a in p for b in u for c in z if a + b + c == s)
+
+# -----------------------------------------------------------
+# License
+# Tasks are the property of Codewars (https://www.codewars.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
+# 
+# Copyright (C) 2022 Vladimir Rukavishnikov
+# 
+# This file is part of the HungryVovka/Codewars-Python
+# (https://github.com/HungryVovka/Codewars-Python)
+# 
+# License is GNU General Public License v3.0
+# (https://github.com/HungryVovka/Codewars-Python/blob/main/LICENSE.md)
+# 
+# You should have received a copy of the GNU General Public License v3.0
+# along with this code. If not, see http://www.gnu.org/licenses/
+# -----------------------------------------------------------
