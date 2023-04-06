be57e7b14220393d49ea1068ddc12d4469b5e1bb
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-06 23:50:59 +0500

    feat: 8 kyu solutions

--- 8 kyu/Sum The Strings.py
+++ 8 kyu/Sum The Strings.py
@@ -31,3 +31,24 @@ def sum_str(a, b):
 def sum_str(a, b):
     sum_string = int(a or 0) + int(b or 0)
     return str(sum_string)
+
+# -----------------------------------------------------------
+# License
+# Tasks are the property of Codewars (https://www.codewars.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
+# 
+# Copyright (C) 2022 Vladimir Rukavishnikov
+# 
+# This file is part of the HungryVovka/Codewars-Python
+# (https://github.com/HungryVovka/Codewars-Python)
+# 
+# License is GNU General Public License v3.0
+# (https://github.com/HungryVovka/Codewars-Python/blob/main/LICENSE.md)
+# 
+# You should have received a copy of the GNU General Public License v3.0
+# along with this code. If not, see http://www.gnu.org/licenses/
+# -----------------------------------------------------------
--- 8 kyu/simple calculator.py
+++ 8 kyu/simple calculator.py
@@ -28,7 +28,7 @@ def calculator(x, y, op):
 
 # or
 
-def calculator(x, y, op):
+def calculator(x,y,op):
     try:
         assert op in "+-*/"
         assert type(x) == int
@@ -36,3 +36,24 @@ def calculator(x, y, op):
         return eval("%d%s%d" % (x, op, y))
     except:
         return "unknown value"
+
+# -----------------------------------------------------------
+# License
+# Tasks are the property of Codewars (https://www.codewars.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
+# 
+# Copyright (C) 2022 Vladimir Rukavishnikov
+# 
+# This file is part of the HungryVovka/Codewars-Python
+# (https://github.com/HungryVovka/Codewars-Python)
+# 
+# License is GNU General Public License v3.0
+# (https://github.com/HungryVovka/Codewars-Python/blob/main/LICENSE.md)
+# 
+# You should have received a copy of the GNU General Public License v3.0
+# along with this code. If not, see http://www.gnu.org/licenses/
+# -----------------------------------------------------------
