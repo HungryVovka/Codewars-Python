897d3198808b1dd4201b15392dab4b36569e0e17
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-10 23:20:24 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/More than Zero.py
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------
+# Correct this code so that it takes one argument, x, and returns "x is more than zero" if x is positive (and nonzero), and otherwise, returns "x is 
+# equal to or less than zero." In both cases, replace x with the actual value of x.
+# -----------------------------------------------------------
+
+def corrections(x):
+    if x <= 0:
+        return f"{x} is equal to or less than zero."
+    else:
+        return f"{x} is more than zero."
+
+# -----------------------------------------------------------
+# License
+# Tasks are the property of Codewars (https://www.codewars.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
+# 
+# Copyright (C) 2022 Vladimir Rukavishnikov
+# 
+# This file is part of the HungryVovka/Codewars-Python
+# (https://github.com/HungryVovka/Codewars-Python)
+# 
+# License is GNU General Public License v3.0
+# (https://github.com/HungryVovka/Codewars-Python/blob/main/LICENSE.md)
+# 
+# You should have received a copy of the GNU General Public License v3.0
+# along with this code. If not, see http://www.gnu.org/licenses/
+# -----------------------------------------------------------
