bd83d1ae3ed38233ec5678a0a495f129af471eba
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-05-15 22:24:39 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Up and down, the string grows.py
@@ -0,0 +1,177 @@
+# -----------------------------------------------------------
+# Many programming languages provide the functionality of converting a string to uppercase or lowercase. For example, upcase/downcase in Ruby, 
+# upper/lower in Python, and toUpperCase/toLowerCase in Java/JavaScript. Typically, these methods won't change the size of the string.
+# 
+# For example, in Ruby, str.upcase.downcase.size == str.size is true for most cases.
+# 
+# However, in some special cases, the length of the transformed string may be longer than the original. Can you find a string that satisfies this criteria?
+# 
+# For example, in Ruby, That means str.upcase.downcase.size > str.size
+# 
+# You should just set the value of STRANGE_STRING to meet the previous criteria.
+# 
+# Note: Meta programming is not allowed in this kata. So, the size of your solution is limited.
+# -----------------------------------------------------------
+
+STRANGE_STRING = "ᾲ"
+
+# or
+
+STRANGE_STRING = "ᾳ"
+
+# or
+
+STRANGE_STRING = "ᾴ"
+
+# or
+
+STRANGE_STRING = "ᾶ"
+
+# or
+
+STRANGE_STRING = "ᾷ"
+
+# or
+
+STRANGE_STRING = "ῗ"
+
+# or
+
+STRANGE_STRING = "ῢ"
+
+# or
+
+STRANGE_STRING = "ῧ"
+
+# or
+
+STRANGE_STRING = "ΐ"
+
+# or
+
+STRANGE_STRING = "ᾙ"
+
+# or
+
+STRANGE_STRING = "ß"
+
+# or
+
+STRANGE_STRING = "ﬃ"
+
+# or
+
+STRANGE_STRING = "ᾔ"
+
+# or
+
+STRANGE_STRING = "ῼ"
+
+# or
+
+STRANGE_STRING = "և"
+
+# or
+
+STRANGE_STRING = "ῲ"
+
+# or
+
+STRANGE_STRING = "ẘ"
+
+# or
+
+STRANGE_STRING = "İ"
+
+# or
+
+STRANGE_STRING="\u1F97"
+
+# or
+
+STRANGE_STRING="\u1FB2"
+
+# or
+
+STRANGE_STRING="\u1FB3"
+
+# or
+
+STRANGE_STRING="\u1FB4"
+
+# or
+
+STRANGE_STRING="\u1FB7"
+
+# or
+
+STRANGE_STRING="\u1FE3"
+
+# or
+
+STRANGE_STRING="\u1FE2"
+
+# or
+
+STRANGE_STRING="\u1FE7"
+
+# or
+
+STRANGE_STRING="\u1FD3"
+
+# or
+
+STRANGE_STRING="\u1F99"
+
+# or
+
+STRANGE_STRING="\uFB03"
+
+# or
+
+STRANGE_STRING="\u1F94"
+
+# or
+
+STRANGE_STRING="\u1FFC"
+
+# or
+
+STRANGE_STRING="\u0587"
+
+# or
+
+STRANGE_STRING="\u1FF2"
+
+# or
+
+STRANGE_STRING="\u00DF"
+
+# or
+
+STRANGE_STRING="\u0130"
+
+# or
+
+STRANGE_STRING="\u1E98"
+
+# -----------------------------------------------------------
+# License
+# Tasks are the property of Codewars (https://www.codewars.com/) 
+# and users of this resource.
+# 
+# All solution code in this repository 
+# is the personal property of Vladimir Rukavishnikov
+# (vladimirrukavishnikovmail@gmail.com).
+# 
+# Copyright (C) 2022 Vladimir Rukavishnikov
+# 
+# This file is part of the HungryVovka/Codewars-Python
+# (https://github.com/HungryVovka/Codewars-Python)
+# 
+# License is GNU General Public License v3.0
+# (https://github.com/HungryVovka/Codewars-Python/blob/main/LICENSE.md)
+# 
+# You should have received a copy of the GNU General Public License v3.0
+# along with this code. If not, see http://www.gnu.org/licenses/
+# -----------------------------------------------------------
