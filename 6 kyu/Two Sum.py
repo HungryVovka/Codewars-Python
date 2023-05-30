fe9ef59eefbdbd6f27d6d1787159704b3652b227
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-05-30 14:35:35 +0500

    feat: 6 kyu solutions

--- 6 kyu/Two Sum.py
+++ 6 kyu/Two Sum.py
@@ -19,6 +19,14 @@ def two_sum(numbers, target):
         if deduction in numbers and numbers.index(deduction) != i:
             return [i, numbers.index(deduction)]
 
+# or
+
+def two_sum(numbers, target):
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return [i, j]
+
 # -----------------------------------------------------------
 # License
 # Tasks are the property of Codewars (https://www.codewars.com/) 
