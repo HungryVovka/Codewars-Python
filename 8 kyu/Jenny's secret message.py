502b3e2ab41d206dd2c7d27e7fe7dfd0e9db9d88
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-06-04 23:27:17 +0500

    feat: 8 kyu solutions

--- 8 kyu/Jenny's secret message.py
+++ 8 kyu/Jenny's secret message.py
@@ -9,7 +9,7 @@ def greet(name):
     if name == "Johnny":
         return "Hello, my love!"
     else:
-        return "Hello, {name}!".format(name=name)
+        return "Hello, {name}!".format(name = name)
 
 # -----------------------------------------------------------
 # License
